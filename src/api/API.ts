897478fcbed2fd20e939337f3670ad70bd63faff
hyojinLeee-git <splato8899@gmail.com>
2023-10-09 17:48:48 +0900

    refactor: api get 함수 수정, post 추가

--- src/api/API.ts
+++ src/api/API.ts
@@ -2,12 +2,25 @@ import { AxiosRequestConfig } from "axios";
 import axiosInstance from "./axios";
 
 export default class API {
-    static async get(
+    static get<T>(
         url: string,
         params?: Pick<AxiosRequestConfig, "params">,
         config?: Omit<AxiosRequestConfig, "params">
-    ) {
-        const res = await axiosInstance.get(url, { ...config, params });
-        return res;
+    ): Promise<T> {
+        const option: AxiosRequestConfig = {
+            method: "get",
+            url,
+            params,
+            ...config,
+        };
+        return axiosInstance(option);
+    }
+    static post<T, D>(url: string, params: T): Promise<D> {
+        const option: AxiosRequestConfig = {
+            method: "post",
+            url,
+            params,
+        };
+        return axiosInstance(option);
     }
 }
