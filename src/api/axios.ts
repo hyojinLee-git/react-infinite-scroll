57e52d4f028900694611c8cdc42e18ca0f525176
hyojinLeee-git <splato8899@gmail.com>
2023-10-09 17:29:37 +0900

    feat: add res axios interceptor

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,17 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_MSW_URL,
 });
+
+const handleResFulfilled = (res: AxiosResponse<any, any>) => {
+    return res.data;
+};
+
+const handleResError = (err: any) => {
+    return Promise.reject(err);
+};
+
+axiosInstance.interceptors.response.use(handleResFulfilled, handleResError);
 
 export default axiosInstance;
