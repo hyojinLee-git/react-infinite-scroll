a19a00eddf4e6713ce6675fa4a8aa83ae0d0caef
hyojinLeee-git <splato8899@gmail.com>
2023-10-09 17:21:59 +0900

    feat: add test msw function

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,18 @@
+import API from "api/API";
+import { URLs } from "api/scripts";
+import { useEffect } from "react";
+
 function App() {
+    const fetchTodo = async () => {
+        const res = await API.get(URLs.mswTest.todos);
+        console.log(res);
+        return res;
+    };
+
+    useEffect(() => {
+        fetchTodo();
+    }, []);
+
     return <div></div>;
 }
 
